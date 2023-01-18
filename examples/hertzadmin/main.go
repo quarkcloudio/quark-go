591225b70526126f3cc9a357a4b657551f685e80
tangtanglove <dai_hang_love@126.com>
2023-01-18 16:36:03 +0800

    chore: 修正hertz实例

--- examples/hertzadmin/main.go
+++ examples/hertzadmin/main.go
@@ -5,6 +5,13 @@ package main
 import (
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/quarkcms/quark-go/pkg/adapter/hertzadapter"
+	"github.com/quarkcms/quark-go/pkg/app/handler/admin"
+	"github.com/quarkcms/quark-go/pkg/app/install"
+	"github.com/quarkcms/quark-go/pkg/app/middleware"
+	"github.com/quarkcms/quark-go/pkg/builder"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -16,6 +23,31 @@ func main() {
 
 	// 注册路由
 	register(h)
+
+	// 数据库配置信息
+	dsn := "root:Bc5HQFJc4bLjZCcC@tcp(127.0.0.1:3306)/quarkgo?charset=utf8&parseTime=True&loc=Local"
+
+	// 配置资源
+	config := &builder.Config{
+		AppKey:    "123456",
+		Providers: admin.Providers,
+		DBConfig: &builder.DBConfig{
+			Dialector: mysql.Open(dsn),
+			Opts:      &gorm.Config{},
+		},
+	}
+
+	// 创建对象
+	b := builder.New(config)
+
+	// 初始化安装
+	b.Use(install.Handle)
+
+	// 中间件
+	b.Use(middleware.Handle)
+
+	// 适配hertz
+	hertzadapter.Adapter(b, h)
 
 	// 启动服务
 	h.Spin()
--- examples/hertzadmin/router.go
+++ examples/hertzadmin/router.go
@@ -4,41 +4,9 @@ package main
 
 import (
 	"github.com/cloudwego/hertz/pkg/app/server"
-	"github.com/quarkcms/quark-go/examples/hertzadmin/biz/handler"
-	"github.com/quarkcms/quark-go/pkg/adapter/hertzadapter"
-	"github.com/quarkcms/quark-go/pkg/app/handler/admin"
-	"github.com/quarkcms/quark-go/pkg/app/install"
-	"github.com/quarkcms/quark-go/pkg/app/middleware"
-	"github.com/quarkcms/quark-go/pkg/builder"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 // customizeRegister registers customize routers.
 func customizedRegister(r *server.Hertz) {
 
-	// 数据库配置信息
-	dsn := "root:Bc5HQFJc4bLjZCcC@tcp(127.0.0.1:3306)/quarkgo?charset=utf8&parseTime=True&loc=Local"
-
-	// 配置资源
-	config := &builder.Config{
-		AppKey:    "123456",
-		Providers: append(admin.Providers, handler.Providers...),
-		DBConfig: &builder.DBConfig{
-			Dialector: mysql.Open(dsn),
-			Opts:      &gorm.Config{},
-		},
-	}
-
-	// 创建对象
-	b := builder.New(config)
-
-	// 初始化安装
-	b.Use(install.Handle)
-
-	// 中间件
-	b.Use(middleware.Handle)
-
-	// 适配hertz
-	hertzadapter.Adapter(b, r)
 }
