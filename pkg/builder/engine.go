335cd6a13b9a33ef593a29a3877e6645c0b4c030
tangtanglove <dai_hang_love@126.com>
2023-02-16 14:08:51 +0800

    chore: 发布版本

--- pkg/builder/engine.go
+++ pkg/builder/engine.go
@@ -19,7 +19,7 @@ const (
 	AppName = "QuarkGo"
 
 	// Version of current package
-	Version = "1.1.8"
+	Version = "1.1.9"
 
 	// 静态文件URL
 	RespositoryURL = "https://github.com/quarkcms/quark-go/tree/main/website/"
