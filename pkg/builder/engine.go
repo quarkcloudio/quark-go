ccb1c13aa0fce60b77452e2db849c563b7e007da
quarkcms <869716224@qq.com>
2023-02-18 22:45:25 +0800

    fix: 修复bug

--- pkg/builder/engine.go
+++ pkg/builder/engine.go
@@ -19,7 +19,7 @@ const (
 	AppName = "QuarkGo"
 
 	// Version of current package
-	Version = "1.1.18"
+	Version = "1.1.19"
 
 	// 静态文件URL
 	RespositoryURL = "https://github.com/quarkcms/quark-go/tree/main/website/"
