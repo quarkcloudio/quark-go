efd55aa5df0c35f846c5783ebf08d474072025fa
tangtanglove <dai_hang_love@126.com>
2023-03-08 15:44:53 +0800

    fix: 修正代码

--- pkg/builder/engine.go
+++ pkg/builder/engine.go
@@ -19,7 +19,7 @@ const (
 	AppName = "QuarkGo"
 
 	// Version of current package
-	Version = "1.1.27"
+	Version = "1.1.28"
 
 	// 静态文件URL
 	RespositoryURL = "https://github.com/quarkcms/quark-go/tree/main/website/"
