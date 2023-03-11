e70c94920cb778545435408b6546a5d46b16af72
tangtanglove <dai_hang_love@126.com>
2023-03-11 09:43:44 +0800

    fix: 修复bug

--- pkg/builder/engine.go
+++ pkg/builder/engine.go
@@ -19,7 +19,7 @@ const (
 	AppName = "QuarkGo"
 
 	// Version of current package
-	Version = "1.1.33"
+	Version = "1.1.34"
 
 	// 静态文件URL
 	RespositoryURL = "https://github.com/quarkcms/quark-go/tree/main/website/"
--- pkg/builder/template/adminpage/adminpage.go
+++ pkg/builder/template/adminpage/adminpage.go
@@ -1,8 +1,12 @@
 package adminpage
 
 import (
+	"github.com/quarkcms/quark-go/pkg/app/model"
 	"github.com/quarkcms/quark-go/pkg/builder"
 	"github.com/quarkcms/quark-go/pkg/builder/template"
+	"github.com/quarkcms/quark-go/pkg/component/admin/footer"
+	"github.com/quarkcms/quark-go/pkg/component/admin/layout"
+	"github.com/quarkcms/quark-go/pkg/msg"
 )
 
 // 后台登录模板
@@ -32,9 +36,46 @@ func (p *Template) Render(ctx *builder.Context) interface{} {
 		"component": ctx.Param("component"),
 	}
 
-	component := ctx.Template.(interface {
-		PageComponentRender(ctx *builder.Context, body interface{}) interface{}
-	}).PageComponentRender(ctx, data)
+	admin := &model.Admin{}
+	config := ctx.Engine.GetConfig()
+
+	// 获取登录管理员信息
+	adminInfo, err := admin.GetAuthUser(config.AppKey, ctx.Token())
+	if err != nil {
+		return ctx.JSON(200, msg.Error(err.Error(), ""))
+	}
+
+	// 获取管理员菜单
+	getMenus, err := admin.GetMenuListById(adminInfo.Id)
+	if err != nil {
+		return ctx.JSON(200, msg.Error(err.Error(), ""))
+	}
+
+	adminLayout := ctx.Engine.GetAdminLayout()
+
+	// 页脚
+	footer := (&footer.Component{}).
+		Init().
+		SetCopyright(adminLayout.Copyright).
+		SetLinks(adminLayout.Links)
+
+	component := (&layout.Component{}).
+		Init().
+		SetTitle(adminLayout.Title).
+		SetLogo(adminLayout.Logo).
+		SetActions(adminLayout.Actions).
+		SetLayout(adminLayout.Layout).
+		SetSplitMenus(adminLayout.SplitMenus).
+		SetContentWidth(adminLayout.ContentWidth).
+		SetPrimaryColor(adminLayout.PrimaryColor).
+		SetFixSiderbar(adminLayout.FixSiderbar).
+		SetFixedHeader(adminLayout.FixedHeader).
+		SetIconfontUrl(adminLayout.IconfontUrl).
+		SetLocale(adminLayout.Locale).
+		SetSiderWidth(adminLayout.SiderWidth).
+		SetMenu(getMenus).
+		SetBody(data).
+		SetFooter(footer)
 
 	return ctx.JSON(200, component)
 }
