c2297cdf302b29d226e4a8ec400f18598d98d043
tangtanglove <dai_hang_love@126.com>
2023-03-31 15:38:56 +0800

    fix: 修复文件上传bug

--- pkg/builder/engine.go
+++ pkg/builder/engine.go
@@ -19,7 +19,7 @@ const (
 	AppName = "QuarkGo"
 
 	// Version of current package
-	Version = "1.1.46"
+	Version = "1.1.47"
 
 	// 静态文件URL
 	RespositoryURL = "https://github.com/quarkcms/quark-go/tree/main/website/"
