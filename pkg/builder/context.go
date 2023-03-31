92cd18d62489900125d2af95de2fb88d7716aaac
tangtanglove <dai_hang_love@126.com>
2023-03-31 12:40:56 +0800

    fix: 修复bug

--- pkg/builder/context.go
+++ pkg/builder/context.go
@@ -13,19 +13,21 @@ import (
 
 	"github.com/derekstavis/go-qs"
 	"github.com/gobeam/stringy"
+	"github.com/labstack/echo/v4"
 	"github.com/quarkcms/quark-go/pkg/msg"
 )
 
 // Context is the most important part of gin. It allows us to pass variables between middleware,
 // manage the flow, validate the JSON of a request and render a JSON response for example.
 type Context struct {
-	Engine   *Engine                // 引擎实例
-	Request  *http.Request          // Request
-	Writer   http.ResponseWriter    // ResponseWriter
-	Template interface{}            // 资源模板实例
-	fullPath string                 // 路由
-	Params   map[string]string      // URL param
-	Querys   map[string]interface{} // URL querys
+	Engine      *Engine                // 引擎实例
+	EchoContext echo.Context           // Echo框架上下文
+	Request     *http.Request          // Request
+	Writer      http.ResponseWriter    // ResponseWriter
+	Template    interface{}            // 资源模板实例
+	fullPath    string                 // 路由
+	Params      map[string]string      // URL param
+	Querys      map[string]interface{} // URL querys
 }
 
 type ParamValue struct {
@@ -41,195 +43,195 @@ func (p *Context) ContentType() string {
 
 // IsTLS returns true if HTTP connection is TLS otherwise false.
 func (p *Context) IsTLS() bool {
-	return p.Engine.echoContext.IsTLS()
+	return p.EchoContext.IsTLS()
 }
 
 // IsWebSocket returns true if HTTP connection is WebSocket otherwise false.
 func (p *Context) IsWebSocket() bool {
-	return p.Engine.echoContext.IsWebSocket()
+	return p.EchoContext.IsWebSocket()
 }
 
 // Scheme returns the HTTP protocol scheme, `http` or `https`.
 func (p *Context) Scheme() string {
-	return p.Engine.echoContext.Scheme()
+	return p.EchoContext.Scheme()
 }
 
 // RealIP returns the client's network address based on `X-Forwarded-For`
 // or `X-Real-IP` request header.
 // The behavior can be configured using `Echo#IPExtractor`.
 func (p *Context) RealIP() string {
-	return p.Engine.echoContext.RealIP()
+	return p.EchoContext.RealIP()
 }
 
 // QueryParam returns the query param for the provided name.
 func (p *Context) QueryParam(name string) string {
-	return p.Engine.echoContext.QueryParam(name)
+	return p.EchoContext.QueryParam(name)
 }
 
 // QueryParams returns the query parameters as `url.Values`.
 func (p *Context) QueryParams() url.Values {
-	return p.Engine.echoContext.QueryParams()
+	return p.EchoContext.QueryParams()
 }
 
 // QueryString returns the URL query string.
 func (p *Context) QueryString() string {
-	return p.Engine.echoContext.QueryString()
+	return p.EchoContext.QueryString()
 }
 
 // FormValue returns the form field value for the provided name.
 func (p *Context) FormValue(name string) string {
-	return p.Engine.echoContext.FormValue(name)
+	return p.EchoContext.FormValue(name)
 }
 
 // FormParams returns the form parameters as `url.Values`.
 func (p *Context) FormParams() (url.Values, error) {
-	return p.Engine.echoContext.FormParams()
+	return p.EchoContext.FormParams()
 }
 
 // FormFile returns the multipart form file for the provided name.
 func (p *Context) FormFile(name string) (*multipart.FileHeader, error) {
-	return p.Engine.echoContext.FormFile(name)
+	return p.EchoContext.FormFile(name)
 }
 
 // MultipartForm returns the multipart form.
 func (p *Context) MultipartForm() (*multipart.Form, error) {
-	return p.Engine.echoContext.MultipartForm()
+	return p.EchoContext.MultipartForm()
 }
 
 // Cookie returns the named cookie provided in the request.
 func (p *Context) Cookie(name string) (*http.Cookie, error) {
-	return p.Engine.echoContext.Cookie(name)
+	return p.EchoContext.Cookie(name)
 }
 
 // SetCookie adds a `Set-Cookie` header in HTTP response.
 func (p *Context) SetCookie(cookie *http.Cookie) {
-	p.Engine.echoContext.SetCookie(cookie)
+	p.EchoContext.SetCookie(cookie)
 }
 
 // Cookies returns the HTTP cookies sent with the request.
 func (p *Context) Cookies() []*http.Cookie {
-	return p.Engine.echoContext.Cookies()
+	return p.EchoContext.Cookies()
 }
 
 // Get retrieves data from the context.
 func (p *Context) Get(key string) interface{} {
-	return p.Engine.echoContext.Get(key)
+	return p.EchoContext.Get(key)
 }
 
 // Set saves data in the context.
 func (p *Context) Set(key string, val interface{}) {
-	p.Engine.echoContext.Set(key, val)
+	p.EchoContext.Set(key, val)
 }
 
 // Bind binds the request body into provided type `i`. The default binder
 // does it based on Content-Type header.
 func (p *Context) Bind(i interface{}) error {
-	return p.Engine.echoContext.Bind(i)
+	return p.EchoContext.Bind(i)
 }
 
 // Validate validates provided `i`. It is usually called after `Context#Bind()`.
 // Validator must be registered using `Echo#Validator`.
 func (p *Context) Validate(i interface{}) error {
-	return p.Engine.echoContext.Validate(i)
+	return p.EchoContext.Validate(i)
 }
 
 // Render renders a template with data and sends a text/html response with status
 // code. Renderer must be registered using `Echo.Renderer`.
 func (p *Context) Render(code int, name string, data interface{}) error {
-	return p.Engine.echoContext.Render(code, name, data)
+	return p.EchoContext.Render(code, name, data)
 }
 
 // HTML sends an HTTP response with status code.
 func (p *Context) HTML(code int, html string) error {
-	return p.Engine.echoContext.HTML(code, html)
+	return p.EchoContext.HTML(code, html)
 }
 
 // HTMLBlob sends an HTTP blob response with status code.
 func (p *Context) HTMLBlob(code int, b []byte) error {
-	return p.Engine.echoContext.HTMLBlob(code, b)
+	return p.EchoContext.HTMLBlob(code, b)
 }
 
 // String sends a string response with status code.
 func (p *Context) String(code int, s string) error {
-	return p.Engine.echoContext.String(code, s)
+	return p.EchoContext.String(code, s)
 }
 
 // JSON sends a JSON response with status code.
 func (p *Context) JSON(code int, i interface{}) error {
-	return p.Engine.echoContext.JSON(code, i)
+	return p.EchoContext.JSON(code, i)
 }
 
 // JSONPretty sends a pretty-print JSON with status code.
 func (p *Context) JSONPretty(code int, i interface{}, indent string) error {
-	return p.Engine.echoContext.JSONPretty(code, i, indent)
+	return p.EchoContext.JSONPretty(code, i, indent)
 }
 
 // JSONBlob sends a JSON blob response with status code.
 func (p *Context) JSONBlob(code int, b []byte) error {
-	return p.Engine.echoContext.JSONBlob(code, b)
+	return p.EchoContext.JSONBlob(code, b)
 }
 
 // JSONP sends a JSONP response with status code. It uses `callback` to construct
 // the JSONP payload.
 func (p *Context) JSONP(code int, callback string, i interface{}) error {
-	return p.Engine.echoContext.JSONP(code, callback, i)
+	return p.EchoContext.JSONP(code, callback, i)
 }
 
 // JSONPBlob sends a JSONP blob response with status code. It uses `callback`
 // to construct the JSONP payload.
 func (p *Context) JSONPBlob(code int, callback string, b []byte) error {
-	return p.Engine.echoContext.JSONPBlob(code, callback, b)
+	return p.EchoContext.JSONPBlob(code, callback, b)
 }
 
 // XML sends an XML response with status code.
 func (p *Context) XML(code int, i interface{}) error {
-	return p.Engine.echoContext.XML(code, i)
+	return p.EchoContext.XML(code, i)
 }
 
 // XMLPretty sends a pretty-print XML with status code.
 func (p *Context) XMLPretty(code int, i interface{}, indent string) error {
-	return p.Engine.echoContext.XMLPretty(code, i, indent)
+	return p.EchoContext.XMLPretty(code, i, indent)
 }
 
 // XMLBlob sends an XML blob response with status code.
 func (p *Context) XMLBlob(code int, b []byte) error {
-	return p.Engine.echoContext.XMLBlob(code, b)
+	return p.EchoContext.XMLBlob(code, b)
 }
 
 // Blob sends a blob response with status code and content type.
 func (p *Context) Blob(code int, contentType string, b []byte) error {
-	return p.Engine.echoContext.Blob(code, contentType, b)
+	return p.EchoContext.Blob(code, contentType, b)
 }
 
 // Stream sends a streaming response with status code and content type.
 func (p *Context) Stream(code int, contentType string, r io.Reader) error {
-	return p.Engine.echoContext.Stream(code, contentType, r)
+	return p.EchoContext.Stream(code, contentType, r)
 }
 
 // File sends a response with the content of the file.
 func (p *Context) File(file string) error {
-	return p.Engine.echoContext.File(file)
+	return p.EchoContext.File(file)
 }
 
 // Attachment sends a response as attachment, prompting client to save the
 // file.
 func (p *Context) Attachment(file string, name string) error {
-	return p.Engine.echoContext.Attachment(file, name)
+	return p.EchoContext.Attachment(file, name)
 }
 
 // Inline sends a response as inline, opening the file in the browser.
 func (p *Context) Inline(file string, name string) error {
-	return p.Engine.echoContext.Inline(file, name)
+	return p.EchoContext.Inline(file, name)
 }
 
 // NoContent sends a response with no body and a status code.
 func (p *Context) NoContent(code int) error {
-	return p.Engine.echoContext.NoContent(code)
+	return p.EchoContext.NoContent(code)
 }
 
 // Redirect redirects the request to a provided URL with status code.
 func (p *Context) Redirect(code int, url string) error {
-	return p.Engine.echoContext.Redirect(code, url)
+	return p.EchoContext.Redirect(code, url)
 }
 
 // Error invokes the registered global HTTP error handler. Generally used by middleware.
@@ -238,7 +240,7 @@ func (p *Context) Redirect(code int, url string) error {
 //
 // Avoid using this method in handlers as no middleware will be able to effectively handle errors after that.
 func (p *Context) Error(err error) {
-	p.Engine.echoContext.Error(err)
+	p.EchoContext.Error(err)
 }
 
 // 设置SetFullPath
@@ -286,7 +288,7 @@ func (p *Context) OriginalURL() string {
 
 // IP tries to parse the headers in [X-Real-Ip, X-Forwarded-For]. It calls RemoteIP() under the hood
 func (p *Context) ClientIP() string {
-	return p.Engine.echoContext.RealIP()
+	return p.EchoContext.RealIP()
 }
 
 // BodyParser binds the request body to a struct.
@@ -294,7 +296,7 @@ func (p *Context) ClientIP() string {
 // application/json, application/xml, application/x-www-form-urlencoded, multipart/form-data
 // If none of the content types above are matched, it will return a ErrUnprocessableEntity error
 func (p *Context) BodyParser(i interface{}) error {
-	return p.Engine.echoContext.Bind(i)
+	return p.EchoContext.Bind(i)
 }
 
 // 获取请求头数据
