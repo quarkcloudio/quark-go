cab311c27f72dd4f163c361c46379cb4e3f8476a
tangtanglove <dai_hang_love@126.com>
2023-04-19 16:24:24 +0800

    fix: 修正详情页解析规则

--- pkg/builder/template/adminresource/requests/detail.go
+++ pkg/builder/template/adminresource/requests/detail.go
@@ -1,7 +1,11 @@
 package requests
 
 import (
+	"encoding/json"
+	"fmt"
 	"reflect"
+	"strings"
+	"time"
 
 	"github.com/quarkcms/quark-go/pkg/builder"
 	"github.com/quarkcms/quark-go/pkg/dal/db"
@@ -44,8 +48,60 @@ func (p *DetailRequest) FillData(ctx *builder.Context) map[string]interface{} {
 			Elem().
 			FieldByName("Name").String()
 
-		if result[name] != nil {
-			fields[name] = result[name]
+		// 获取实例的回调函数
+		callback := field.(interface{ GetCallback() interface{} }).GetCallback()
+		if callback != nil {
+			getCallback := callback.(func() interface{})
+			fields[name] = getCallback()
+		} else {
+			if result[name] != nil {
+				var fieldValue interface{}
+				fieldValue = result[name]
+				getV, ok := result[name].(string)
+				if ok {
+					if strings.Contains(getV, "{") {
+						var m map[string]interface{}
+						err := json.Unmarshal([]byte(getV), &m)
+						if err != nil {
+							fmt.Printf("Unmarshal with error: %+v\n", err)
+						}
+						fieldValue = m
+					}
+					if strings.Contains(getV, "[") {
+						var m []interface{}
+						err := json.Unmarshal([]byte(getV), &m)
+						if err != nil {
+							fmt.Printf("Unmarshal with error: %+v\n", err)
+						}
+						fieldValue = m
+					}
+				}
+
+				// 组件名称
+				component := reflect.
+					ValueOf(field).
+					Elem().
+					FieldByName("Component").
+					String()
+				if component == "datetimeField" || component == "dateField" {
+					format := reflect.
+						ValueOf(field).
+						Elem().
+						FieldByName("Format").
+						String()
+
+					format = strings.Replace(format, "YYYY", "2006", -1)
+					format = strings.Replace(format, "MM", "01", -1)
+					format = strings.Replace(format, "DD", "02", -1)
+					format = strings.Replace(format, "HH", "15", -1)
+					format = strings.Replace(format, "mm", "04", -1)
+					format = strings.Replace(format, "ss", "05", -1)
+
+					fieldValue = result[name].(time.Time).Format(format)
+				}
+
+				fields[name] = fieldValue
+			}
 		}
 	}
 
