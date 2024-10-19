85f7eaa07a393162ba26c2fd5c7b7e4b0ec2b995
tangtanglove <dai_hang_love@126.com>
2024-10-19 17:46:25 +0800

    chore: wip

--- pkg/app/admin/service/resources/account.go
+++ pkg/app/admin/service/resources/account.go
@@ -39,15 +39,15 @@ func (p *Account) Fields(ctx *builder.Context) []interface{} {
 		field.Image("avatar", "头像"),
 		field.Text("nickname", "昵称").
 			SetRules([]*rule.Rule{
-				rule.New().SetRequired().SetMessage("昵称必须填写"),
+				rule.Required(true, "昵称必须填写"),
 			}),
 		field.Text("email", "邮箱").
 			SetRules([]*rule.Rule{
-				rule.New().SetRequired().SetMessage("邮箱必须填写"),
+				rule.Required(true, "邮箱必须填写"),
 			}),
 		field.Text("phone", "手机号").
 			SetRules([]*rule.Rule{
-				rule.New().SetRequired().SetMessage("手机号必须填写"),
+				rule.Required(true, "手机号必须填写"),
 			}),
 		field.Radio("sex", "性别").
 			SetOptions([]*radio.Option{
