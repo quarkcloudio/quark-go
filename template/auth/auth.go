760c50e14198206459e9486a857522f7e0100463
tangtanglove <dai_hang_love@126.com>
2025-08-31 12:22:30 +0800

    chore: wip

--- template/auth/auth.go
+++ template/auth/auth.go
@@ -163,21 +163,344 @@ func (p *Template) Login(ctx *quark.Context) error {
 
 // 获取用户信息
 func (p *Template) UserInfo(ctx *quark.Context) error {
-	return ctx.JSONOk("请求成功", map[string]interface{}{
-		"id":       1,
-		"username": "QuarkGo",
-		"email":    "admin@quarkgo.com",
-		"avatar":   "https://img2.baidu.com/it/u=3422282222,2328894238&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
-	})
+	data := map[string]interface{}{
+		"id":       "0",
+		"username": "quark",
+		"roles": []string{
+			"R_SUPER",
+		},
+		"buttons": []string{
+			"B_CODE1",
+			"B_CODE2",
+			"B_CODE3",
+		},
+	}
+
+	return ctx.JSONOk("请求成功", data)
 }
 
 // 获取用户路由
 func (p *Template) UserRoutes(ctx *quark.Context) error {
-	return ctx.JSONOk("请求成功", map[string]interface{}{
-		"routes": []string{
-			"/api/admin/dashboard/index/index",
+	data := map[string]interface{}{
+		"routes": []map[string]interface{}{
+			{
+				"name":      "exception",
+				"path":      "/exception",
+				"component": "layout.base",
+				"meta": map[string]interface{}{
+					"title":   "exception",
+					"i18nKey": "route.exception",
+					"icon":    "ant-design:exception-outlined",
+					"order":   7,
+				},
+				"children": []map[string]interface{}{
+					{
+						"name":      "exception_403",
+						"path":      "/exception/403",
+						"component": "view.403",
+						"meta": map[string]interface{}{
+							"title":   "exception_403",
+							"i18nKey": "route.exception_403",
+							"icon":    "ic:baseline-block",
+						},
+					},
+					{
+						"name":      "exception_404",
+						"path":      "/exception/404",
+						"component": "view.404",
+						"meta": map[string]interface{}{
+							"title":   "exception_404",
+							"i18nKey": "route.exception_404",
+							"icon":    "ic:baseline-web-asset-off",
+						},
+					},
+					{
+						"name":      "exception_500",
+						"path":      "/exception/500",
+						"component": "view.500",
+						"meta": map[string]interface{}{
+							"title":   "exception_500",
+							"i18nKey": "route.exception_500",
+							"icon":    "ic:baseline-wifi-off",
+						},
+					},
+				},
+			},
+			{
+				"name":      "about",
+				"path":      "/about",
+				"component": "layout.base$view.about",
+				"meta": map[string]interface{}{
+					"title":   "about",
+					"i18nKey": "route.about",
+					"icon":    "fluent:book-information-24-regular",
+					"order":   10,
+				},
+			},
+			{
+				"name":      "function",
+				"path":      "/function",
+				"component": "layout.base",
+				"meta": map[string]interface{}{
+					"title":   "function",
+					"i18nKey": "route.function",
+					"icon":    "icon-park-outline:all-application",
+					"order":   6,
+				},
+				"children": []map[string]interface{}{
+					{
+						"name":     "function_hide-child",
+						"path":     "/function/hide-child",
+						"redirect": "/function/hide-child/one",
+						"meta": map[string]interface{}{
+							"title":   "function_hide-child",
+							"i18nKey": "route.function_hide-child",
+							"icon":    "material-symbols:filter-list-off",
+							"order":   2,
+						},
+						"children": []map[string]interface{}{
+							{
+								"name":      "function_hide-child_one",
+								"path":      "/function/hide-child/one",
+								"component": "view.function_hide-child_one",
+								"meta": map[string]interface{}{
+									"title":      "function_hide-child_one",
+									"i18nKey":    "route.function_hide-child_one",
+									"icon":       "material-symbols:filter-list-off",
+									"hideInMenu": true,
+									"activeMenu": "function_hide-child",
+								},
+							},
+							{
+								"name":      "function_hide-child_three",
+								"path":      "/function/hide-child/three",
+								"component": "view.function_hide-child_three",
+								"meta": map[string]interface{}{
+									"title":      "function_hide-child_three",
+									"i18nKey":    "route.function_hide-child_three",
+									"hideInMenu": true,
+									"activeMenu": "function_hide-child",
+								},
+							},
+							{
+								"name":      "function_hide-child_two",
+								"path":      "/function/hide-child/two",
+								"component": "view.function_hide-child_two",
+								"meta": map[string]interface{}{
+									"title":      "function_hide-child_two",
+									"i18nKey":    "route.function_hide-child_two",
+									"hideInMenu": true,
+									"activeMenu": "function_hide-child",
+								},
+							},
+						},
+					},
+					{
+						"name":      "function_multi-tab",
+						"path":      "/function/multi-tab",
+						"component": "view.function_multi-tab",
+						"meta": map[string]interface{}{
+							"title":      "function_multi-tab",
+							"i18nKey":    "route.function_multi-tab",
+							"icon":       "ic:round-tab",
+							"multiTab":   true,
+							"hideInMenu": true,
+							"activeMenu": "function_tab",
+						},
+					},
+					{
+						"name":      "function_request",
+						"path":      "/function/request",
+						"component": "view.function_request",
+						"meta": map[string]interface{}{
+							"title":   "function_request",
+							"i18nKey": "route.function_request",
+							"icon":    "carbon:network-overlay",
+							"order":   3,
+						},
+					},
+					{
+						"name":      "function_super-page",
+						"path":      "/function/super-page",
+						"component": "view.function_super-page",
+						"meta": map[string]interface{}{
+							"title":   "function_super-page",
+							"i18nKey": "route.function_super-page",
+							"icon":    "ic:round-supervisor-account",
+							"order":   5,
+						},
+					},
+					{
+						"name":      "function_tab",
+						"path":      "/function/tab",
+						"component": "view.function_tab",
+						"meta": map[string]interface{}{
+							"title":   "function_tab",
+							"i18nKey": "route.function_tab",
+							"icon":    "ic:round-tab",
+							"order":   1,
+						},
+					},
+					{
+						"name":      "function_toggle-auth",
+						"path":      "/function/toggle-auth",
+						"component": "view.function_toggle-auth",
+						"meta": map[string]interface{}{
+							"title":   "function_toggle-auth",
+							"i18nKey": "route.function_toggle-auth",
+							"icon":    "ic:round-construction",
+							"order":   4,
+						},
+					},
+				},
+			},
+			{
+				"name":      "home",
+				"path":      "/home",
+				"component": "layout.base$view.home",
+				"meta": map[string]interface{}{
+					"title":   "home",
+					"i18nKey": "route.home",
+					"icon":    "mdi:monitor-dashboard",
+					"order":   1,
+				},
+			},
+			{
+				"name":      "manage",
+				"path":      "/manage",
+				"component": "layout.base",
+				"meta": map[string]interface{}{
+					"title":   "manage",
+					"i18nKey": "route.manage",
+					"icon":    "carbon:cloud-service-management",
+					"order":   9,
+				},
+				"children": []map[string]interface{}{
+					{
+						"name":      "manage_menu",
+						"path":      "/manage/menu",
+						"component": "view.manage_menu",
+						"meta": map[string]interface{}{
+							"title":     "manage_menu",
+							"i18nKey":   "route.manage_menu",
+							"icon":      "material-symbols:route",
+							"order":     3,
+							"keepAlive": true,
+						},
+					},
+					{
+						"name":      "manage_role",
+						"path":      "/manage/role",
+						"component": "view.manage_role",
+						"meta": map[string]interface{}{
+							"title":   "manage_role",
+							"i18nKey": "route.manage_role",
+							"icon":    "carbon:user-role",
+							"order":   2,
+						},
+					},
+					{
+						"name":      "manage_user",
+						"path":      "/manage/user",
+						"component": "view.manage_user",
+						"meta": map[string]interface{}{
+							"title":   "manage_user",
+							"i18nKey": "route.manage_user",
+							"icon":    "ic:round-manage-accounts",
+							"order":   1,
+						},
+					},
+					{
+						"name":      "manage_user-detail",
+						"path":      "/manage/user-detail/:id",
+						"component": "view.manage_user-detail",
+						"props":     true,
+						"meta": map[string]interface{}{
+							"title":      "manage_user-detail",
+							"i18nKey":    "route.manage_user-detail",
+							"hideInMenu": true,
+							"activeMenu": "manage_user",
+						},
+					},
+				},
+			},
+			{
+				"name":      "multi-menu",
+				"path":      "/multi-menu",
+				"component": "layout.base",
+				"meta": map[string]interface{}{
+					"title":   "multi-menu",
+					"i18nKey": "route.multi-menu",
+					"order":   8,
+				},
+				"children": []map[string]interface{}{
+					{
+						"name": "multi-menu_first",
+						"path": "/multi-menu/first",
+						"meta": map[string]interface{}{
+							"title":   "multi-menu_first",
+							"i18nKey": "route.multi-menu_first",
+							"order":   1,
+						},
+						"children": []map[string]interface{}{
+							{
+								"name":      "multi-menu_first_child",
+								"path":      "/multi-menu/first/child",
+								"component": "view.multi-menu_first_child",
+								"meta": map[string]interface{}{
+									"title":   "multi-menu_first_child",
+									"i18nKey": "route.multi-menu_first_child",
+								},
+							},
+						},
+					},
+					{
+						"name": "multi-menu_second",
+						"path": "/multi-menu/second",
+						"meta": map[string]interface{}{
+							"title":   "multi-menu_second",
+							"i18nKey": "route.multi-menu_second",
+							"order":   2,
+						},
+						"children": []map[string]interface{}{
+							{
+								"name": "multi-menu_second_child",
+								"path": "/multi-menu/second/child",
+								"meta": map[string]interface{}{
+									"title":   "multi-menu_second_child",
+									"i18nKey": "route.multi-menu_second_child",
+								},
+								"children": []map[string]interface{}{
+									{
+										"name":      "multi-menu_second_child_home",
+										"path":      "/multi-menu/second/child/home",
+										"component": "view.multi-menu_second_child_home",
+										"meta": map[string]interface{}{
+											"title":   "multi-menu_second_child_home",
+											"i18nKey": "route.multi-menu_second_child_home",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				"name":      "user-center",
+				"path":      "/user-center",
+				"component": "layout.base$view.user-center",
+				"meta": map[string]interface{}{
+					"title":      "user-center",
+					"i18nKey":    "route.user-center",
+					"hideInMenu": true,
+				},
+			},
 		},
-	})
+		"home": "home",
+	}
+
+	return ctx.JSONOk("请求成功", data)
 }
 
 // 退出方法
