4eb2c9466d330aef32932579f9b2bbe0918cb9cf
tangtanglove <dai_hang_love@126.com>
2026-01-17 13:40:27 +0800

    chore: wip

--- template/resource/actions/batch.go
+++ template/resource/actions/batch.go
@@ -4,6 +4,8 @@ import "github.com/quarkcloudio/quark-go/v4"
 
 type Batch struct {
 	Action
+	Href   string `json:"href"`   // 获取跳转链接
+	Target string `json:"target"` // 相当于 a 链接的 target 属性，href 存在时生效，_blank | _self | _parent | _top
 }
 
 // 初始化
@@ -14,4 +16,14 @@ func (p *Batch) New(ctx *quark.Context) interface{} {
 	p.Size = "small"
 
 	return p
+}
+
+// 获取跳转链接
+func (p *Batch) GetHref(ctx *quark.Context) string {
+	return p.Href
+}
+
+// 相当于 a 链接的 target 属性，href 存在时生效
+func (p *Batch) GetTarget(ctx *quark.Context) string {
+	return p.Target
 }
